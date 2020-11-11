9f1128a45244c3c43390d86876a551484c05c119
Maciej <maciej.lewandowski@student.manchester.ac.uk>
2020-11-11 17:52:46 +0000

    PoC websocket frontend backend

---
+++ echo_handler.hpp
@@ -0,0 +1,8 @@
+
+class echo_handler : public server::handler
+{
+    void on_message(connection_ptr con, std::string msg)
+    {
+        con->write(msg);
+    }
+};
---
+++ install_webapp.sh
@@ -0,0 +1,7 @@
+# install header only websocketpp library
+# works on ubuntu. For installing you need admin access
+git clone https://github.com/manchester-hyperloop/websocketpp
+cd websocketapp
+cmake .
+sudo make install
+cd ..
