acaaab754f756c3a659a108a71e2c9d8ef4d8d8e
AndreiPopescu21 <andrei.popescu2116@icloud.com>
2021-04-16 00:21:33 +0300

    Improvement in the UI and addition to the install file

--- rosbridge/Frontend/src/Pages/AdminPage.js
+++ rosbridge/Frontend/src/Pages/AdminPage.js
@@ -4,12 +4,20 @@ import { useHistory } from "react-router";
 import AdminUserRequests from "../Components/AdminUserRequests";
 import AdminUserManager from "../Components/AdminUserManager";
 import HyperloopTitleImage from '../Images/HyperloopTitle.png';
+import AdminSidebar from '../Components/AdminSidebar';
+
+const drawerBigWidth = 300;
+const drawerSmallWidth = 50;
+const marginWindowWidth = 1200;
 
 const useStyles = makeStyles((theme) =>
     {
         return{
         paper: {
             textAlign: "left",
+        },
+        mainContainer: {
+            display: 'flex',
         }
     }
 });
@@ -19,6 +27,28 @@ const AdminPage = () => {
     const styles = useStyles();
 
     const [currentPage, setCurrentPage] = useState("");
+    const [isScreenMaximized, setIsScreenMaximized] = useState(true);
+    const [windowSize, setWindowSize] = useState([window.innerWidth, window.innerHeight]);
+    
+    // The function that triggers when the window is resized
+    const handleWindowResize = () => {
+        setWindowSize([window.innerWidth, window.innerHeight]);
+    }
+
+    // Load resize event when the page is loaded
+    useEffect(() => {
+        window.addEventListener("resize", handleWindowResize);
+        handleWindowResize();
+        return () => window.removeEventListener("resize", handleWindowResize);
+    },[]);
+
+    // When the window is resized, check if it is less than the margin value
+    useEffect(() => {
+        if(windowSize[0] > marginWindowWidth)
+        setIsScreenMaximized(true);
+        else
+        setIsScreenMaximized(false);
+    },[windowSize]);
     
     useEffect(() => {
         if(window.location.pathname !== '/admin/userrequests' && 
@@ -34,29 +64,39 @@ const AdminPage = () => {
     },[history])
 
     return (
-        <Container>
-            <div style={{textAlign: "center"}}>
-                <img alt="Hyperloop Title" src={HyperloopTitleImage}/>
+        <div className={styles.mainContainer} >
+            <div>
+                <div>
+                <AdminSidebar isScreenMaximized = {isScreenMaximized} drawerBigWidth = {drawerBigWidth} drawerSmallWidth = {drawerSmallWidth}/>
+                </div>
             </div>
 
-            <Typography variant="h4" gutterBottom>
-                { currentPage === "userrequests" ? "User Requests" : "User Manager" }
-            </Typography>
+            <div style={{width: '100%', marginLeft: isScreenMaximized ? drawerBigWidth : drawerSmallWidth}}>
+            <Container>
+                <div style={{textAlign: "center"}}>
+                    <img alt="Hyperloop Title" src={HyperloopTitleImage}/>
+                </div>
 
-            <br/>
+                <Typography variant="h4" gutterBottom>
+                    { currentPage === "userrequests" ? "User Requests" : "User Manager" }
+                </Typography>
 
-            <div style={{textAlign: "center"}}>
-                <Paper elevation={3} className={styles.paper}>
-                    {
-                        currentPage === "userrequests" ?
-                            <AdminUserRequests/>
-                            :
-                            <AdminUserManager/>
-                    }
-                </Paper>
+                <br/>
+
+                <div style={{textAlign: "center"}}>
+                    <Paper elevation={3} className={styles.paper}>
+                        {
+                            currentPage === "userrequests" ?
+                                <AdminUserRequests/>
+                                :
+                                <AdminUserManager/>
+                        }
+                    </Paper>
+                </div>
+                <br/>
+            </Container>
             </div>
-            <br/>
-        </Container>
+        </div>
     );
 }
 
--- rosbridge/install.sh
+++ rosbridge/install.sh
@@ -9,10 +9,10 @@ sudo apt install -y mongodb
 cd ./Frontend
 npm install
 cd ../Backend
+npm install
 read -p "Enter E-mail: " email
 read -p "Enter Username: " username
 read -p 'Enter Password: ' password
 node factory.js $email $username $password
 echo true > valid.txt
-npm install
 cd ..
